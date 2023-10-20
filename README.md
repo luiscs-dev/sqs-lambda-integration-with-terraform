b72febf0117e285cc776710186a755cf8b16cf42
luiscs-dev <luiscs.dev@gmail.com>
2023-10-19 19:34:30 -0600

    Update diagrams

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # sqs-lambda-integration-with-terraform
 
+![General Diagram](resources/diagrams/general_diagram.png)
+
+
 # Terraform
+
+### Commands to know
+```bash
+terraform plan+
+```
+
+```bash
+terraform plan -out tplan.tfplan
+```
+
+```bash
+terraform apply "tplan.tfplan"
+```
+
+```bash
+terraform workspace list
+```
+
+```bash
+terraform workspace new {dev,prod}
+```
+
+```bash
+terraform workspace select {dev,prod}
+
+```bash
+terraform apply -auto-approve
+```
+
+```bash
+terraform destroy -auto-approve
+```
--- main.tf
+++ main.tf
@@ -9,12 +9,15 @@ terraform {
 }
 
 variable "aws_region" {
-    type = string
-    default = "us-east-1"
+    type = map
+    default = {
+        dev = "us-east-1"
+        prod = "us-east-1"
+    }
 }
 
 provider "aws" {
-    region = var.aws_region
+    region = var.aws_region[terraform.workspace]
     profile = "aws-training"
 }
 
@@ -26,7 +29,7 @@ data "archive_file" "myzip" {
 
 resource "aws_lambda_function" "sqs_reader_lambda" {
     filename = "main.zip"
-    function_name = "sqs_reader_lambda"
+    function_name = "sqs_reader_lambda_${terraform.workspace}"
     role = aws_iam_role.sqs_lambda_role.arn
     handler = "main.lambda_handler"
     runtime = "python3.10"
@@ -47,6 +50,27 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "sqs_lambda_role" {
-    name = "sqs_lambda_role"
+    name = "sqs_lambda_role_${terraform.workspace}"
     assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_sqs_queue" "main_queue" {
+    name = "main-queue_${terraform.workspace}"
+    delay_seconds = 30
+    max_message_size = 262144
+}
+
+resource "aws_sqs_queue" "dl_queue" {
+    name = "dl-queue_${terraform.workspace}"
+    delay_seconds = 30
+    max_message_size = 262144
+}
+
+resource "aws_lambda_event_source_mapping" "sqs-lambda-trigger" {
+    event_source_arn = aws_sqs_queue.main_queue.arn
+    function_name = aws_lambda_function.sqs_reader_lambda.arn
+}
+
+resource "aws_s3_bucket" "cs-tf-sqs-lambda-bucket" {
+  bucket = "cs-tf-sqs-lambda-bucket"
 }
